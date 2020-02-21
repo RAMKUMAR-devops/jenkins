5da9b4ff449ad28399ef13cfe52506d2d6c49fa7
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2020-02-21 11:53:26 +0530

    Create jobs.groovy

---
+++ seed/jobs.groovy
@@ -0,0 +1,21 @@
+pipelineJob('example') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/RAMKUMAR-devops/jenkins.git')
+                        'credentialsId'('GitUser1')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('pipeline-jobs/sample.Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}

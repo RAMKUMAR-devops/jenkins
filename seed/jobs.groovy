77faa603443e548c3c91e38dac388d2e87eb4d2d
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2020-02-21 12:00:20 +0530

    Update jobs.groovy

--- seed/jobs.groovy
+++ seed/jobs.groovy
@@ -1,4 +1,21 @@
-pipelineJob('example') {
+
+// Terraform Folder
+folder('Terraform') {
+    displayName('Terraform')
+    description('Folder for Terraform Jobs')
+}
+
+folder('Terraform/ENV') {
+    displayName('ENV')
+    description('Folder for Terraform Jobs for ENV')
+}
+
+folder('Terraform/ENV/DEV') {
+    displayName('DEV')
+    description('Folder for Terraform Jobs for DEV ENV')
+}
+
+pipelineJob('Terraform/ENV/DEV/VPC') {
     configure { flowdefinition ->
         flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
@@ -14,8 +31,10 @@ pipelineJob('example') {
                     }
                 }
             }
-            'scriptPath'('pipeline-jobs/sample.Jenkinsfile')
+            'scriptPath'('pipeline-jobs/terraform/env/dev/vpc.Jenkinsfile')
             'lightweight'(true)
         }
     }
 }
+
+
